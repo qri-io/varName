9c25cdf515250eeb71400888fecd43930462611c
Thomas Osterbind <osterbit@onid.oregonstate.edu>
2017-11-17 17:04:31 -0500

    feat: initial commit

---
+++ camelName_test.go
@@ -0,0 +1,22 @@
+package camelName
+
+import (
+	"fmt"
+	"testing"
+)
+
+// func TestConvertMultiCapsToSingleCaps(t *testing.T) {
+
+// }
+
+// func TestTruncateList(t *testing.T){
+
+// }
+
+// func TestMakeTableName(t *testing.T) {
+
+// }
+
+// func TestMakeNameUnique(t *testing.T) {
+
+// }
